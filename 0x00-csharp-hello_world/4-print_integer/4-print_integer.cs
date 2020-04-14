a91dcdde0f6af8c967ec41cf5cdcd337c736d68f
Miuywu <859@holbertonschool.com>
2020-04-13 17:06:16 -0700

    write a line with variable

---
+++ 0x00-csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _4_print_integer
+{
+    class Program
+    {
+	    static void Main(string[] args)
+	    {
+		    int number = 972;
+            Console.Write(number); 
+		    Console.WriteLine(" Mission Street");
+	    }
+    }
+}
