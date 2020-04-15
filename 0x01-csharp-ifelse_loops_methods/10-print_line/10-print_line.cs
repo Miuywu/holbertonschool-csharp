f8ea6df9873d7770a4501febf8162a6590bf7f70
Miuywu <859@holbertonschool.com>
2020-04-14 23:20:45 -0700

    prints underscores according to length input

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        string l = "";
+        for (int i = 0; i < length; i++)
+            l += l;
+        Console.WriteLine(l);
+    }
+}
