45d5fb21ca075d8870a5f0aaeb5b5f5c2beeb895
Miuywu <859@holbertonschool.com>
2020-04-14 22:16:50 -0700

    write 00 - 99

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string d = "";
+            for (int i = 0; i <= 99; i++)
+            {
+                Console.Write("{0:00}", i);
+                if (i != 99)
+                    d = ", ";
+                else
+                    d = ", \n";
+                Console.Write(d);
+            }
+        }
+    }
+}
