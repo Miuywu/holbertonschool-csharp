da22ca4d298e71f18e7245767a04a7d1d4044a7d
Miuywu <859@holbertonschool.com>
2020-04-14 23:32:21 -0700

    print diagonal shape in C#

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        string l = "";
+        if (length > 1)
+        {
+            Console.WriteLine();
+            return;
+        }
+        for (int i = 0; i < length; i++)
+        {
+            Console.Write(l);
+            Console.WriteLine("\\");
+            l += " ";
+        }
+    }
+}
