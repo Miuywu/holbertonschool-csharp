e749daed4130ac8dee16fe63892b46da41e4e228
Miuywu <859@holbertonschool.com>
2020-04-14 22:39:19 -0700

    write 00 - 99 no repeat digit-combos nor 10s > 1s

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string d = "";
+            for (char i = '0'; i <= '9'; i++)
+            {
+                char b = i++;
+                i--;
+                for (char j = b; j <= '9'; j++)
+                {
+                    if (j == i)
+                        continue;
+                    Console.Write("{0}{1}", i, j);
+                    if ((i != '8') || (j != '9'))
+                        d = ", ";
+                    else
+                    {
+                        d = "\n";
+                        return;
+                    }
+                    Console.Write(d);
+                }
+            }
+        }
+    }
+}
