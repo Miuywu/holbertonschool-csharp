484ad3ef7e96ae6c362af37f69b90bff2db9e178
Miuywu <859@holbertonschool.com>
2020-04-14 21:32:46 -0700

    last digit c# if elif el

---
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        string msg = "";
+        if (number % 10 > 5)
+            msg = "and is greater than 5";
+        else if (number % 10 == 0)
+            msg = "and is 0";
+        else if ((number % 10 < 6) && (number != 0))
+            msg = "and is less than 6 and not 0";
+	    Console.WriteLine("The last digit of {0} is {1} {2}", number, number % 10, msg);
+    }
+}
