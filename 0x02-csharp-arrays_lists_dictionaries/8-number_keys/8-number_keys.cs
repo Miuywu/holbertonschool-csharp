e4feee16b0c4ef33eb70ffaef0af13620f937d31
Miuywu <859@holbertonschool.com>
2020-04-16 23:51:50 -0700

    counts dict objs

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int count = 0;
+
+        foreach (var obj in myDict)
+        {
+            count++;
+        }
+        return count;
+    }
+}
