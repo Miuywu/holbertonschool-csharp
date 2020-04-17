864de43f458a541b4085267ee4550623f951b050
Miuywu <859@holbertonschool.com>
2020-04-17 11:11:39 -0700

    multiplies dict entries by 2

---
+++ 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+    {
+        Dictionary<string, int> dict = new Dictionary<string, int>();
+        if (myDict == null)
+            return null;
+        foreach (var key in myDict.Keys)
+        {
+            dict[key] = myDict[key] * 2;
+        }
+        return dict;
+    }
+}
