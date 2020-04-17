50b1a4bb7672979c3febb1b876797035215fbd5d
Miuywu <859@holbertonschool.com>
2020-04-16 23:42:17 -0700

    returns a bool list

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> liz;
+        if (myList == null)
+            return null;
+        liz = new List<bool>();
+        for (int a = 0; a < myList.Count; a++)
+        {
+            if (myList[a] % 2 == 0)
+                liz.Add(true);
+            else
+                liz.Add(false);
+        }
+        return liz;
+    }
+}
