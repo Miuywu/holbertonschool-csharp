8530e2ba80248ae1f4c7c17d37a1c2b0c3585e98
Miuywu <859@holbertonschool.com>
2020-04-17 11:23:53 -0700

    write entries of dict ordered by key

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        List<string> liz = new List<string>();
+        foreach (string key in myDict.Keys)
+            liz.Add(key);
+        liz.Sort();
+        foreach (string key in liz)
+            Console.WriteLine("{0}: {1}", key, myDict[key]);
+    }
+}
