ab666011132c579438933e3df75490015ff6442a
Miuywu <859@holbertonschool.com>
2020-04-16 23:16:48 -0700

    creates and prints list

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        List<int> liz;
+
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        liz = new List<int>();
+        for (int a = 0; a < size; a++)
+        {
+            liz.Add(a);
+            Console.Write("{0}", a);
+            if (a != size - 1)
+                Console.Write(" ");
+        }
+        Console.WriteLine();
+        return liz;
+    }
+}
