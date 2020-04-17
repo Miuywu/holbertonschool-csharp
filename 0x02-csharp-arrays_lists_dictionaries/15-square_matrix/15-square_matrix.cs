34964da0e6d0090ac5659a63f1f7d82e59547e87
Miuywu <859@holbertonschool.com>
2020-04-17 13:01:03 -0700

    writes matrix with squared values

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+		int[,] ara = new int[myMatrix.GetLength(0), myMatrix.GetLength(1)];
+		for (int a = 0; a < myMatrix.GetLength(0); a++)
+			for (int b = 0; b < myMatrix.GetLength(1); b++)
+				ara[a, b] = myMatrix[a, b] * myMatrix[a, b];
+		return ara;
+    }
+}
