bc15aba120c329d08a2abedcfb346d4dbe0ff52e
Miuywu <859@holbertonschool.com>
2020-04-16 22:28:51 -0700

    creates, prints and returns array

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        int[] ara;
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        ara = new int[size];
+        for (int a = 0; a < size; a++)
+            ara[a] = a;
+        foreach (var ele in ara)
+        {
+            Console.Write("{0}", ele);
+            if (ele != ara[ara.Length - 1])
+                Console.Write(" ");
+            else
+                Console.Write("\n");
+        }
+        return ara;
+    }
+}
