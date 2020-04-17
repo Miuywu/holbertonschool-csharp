9eda5023364925008949be4a21c39df0c09060cf
Miuywu <859@holbertonschool.com>
2020-04-17 12:27:07 -0700

    writes grid

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] ara = new int[5, 5];
+            ara[2, 2] = 1;
+            for (int a = 0; a < 5; a++)
+            {
+                for (int b = 0; b < 5; b++)
+                {
+                    Console.Write("{0}", ara[a, b]);
+                    if (b != 4)
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
