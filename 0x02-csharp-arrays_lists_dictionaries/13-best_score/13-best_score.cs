c673463cb4075779d4b7cbd6564f4103a7f87d31
Miuywu <859@holbertonschool.com>
2020-04-17 11:40:43 -0700

    returns key max value in dict

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList.Count == 0)
+            return null;
+        int m = Int32.MinValue;
+        string k = "";
+        foreach (string key in myList.Keys)
+        {
+            if (m < myList[key])
+            {
+                m = myList[key];
+                k = key;        
+            }        
+        }
+        return k;
+    }
+}
