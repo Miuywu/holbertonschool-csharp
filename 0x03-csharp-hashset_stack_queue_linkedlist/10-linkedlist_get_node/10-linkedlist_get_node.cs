5bbfbac9caff29d7c42a0deaa832cdf546c3894f
Miuywu <859@holbertonschool.com>
2020-04-28 23:56:26 -0700

    noder

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> N;
+
+        if (n < 0 || n > myLList.Count - 1)
+        {
+            return 0;
+        }
+        for (N = myLList.First; n > 0; N = N.Next, n--)
+            ;
+
+        return N.Value;
+    }
+}
