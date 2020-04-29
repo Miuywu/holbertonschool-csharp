85e78472f598cb601ee43fd03157c3fea94a2f15
Miuywu <859@holbertonschool.com>
2020-04-28 23:50:55 -0700

    return first and pop

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        int n;
+
+        if (myLList.Count == 0)
+        {
+            n = 0;
+        }
+        else
+        {
+            n = myLList.First.Value;
+            myLList.RemoveFirst();
+        }
+        return val;
+    }
+}
