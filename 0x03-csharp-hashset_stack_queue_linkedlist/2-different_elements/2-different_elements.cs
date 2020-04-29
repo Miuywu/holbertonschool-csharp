4e24cd32f120db77a9085709a4d4b3abac8bca63
Miuywu <859@holbertonschool.com>
2020-04-28 23:30:50 -0700

    addrange contains

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> liz = list2.FindAll(elem => !list1.Contains(elem));
+		List<int> lizz = list1.FindAll(elem => !list2.Contains(elem));
+        lizz.AddRange(liz);
+		lizz.Sort();
+		return lizz;
+    }
+}
