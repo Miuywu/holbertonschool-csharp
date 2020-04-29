9bd3b72c72aba49d76a4b064e06cf8cb8c862055
Miuywu <859@holbertonschool.com>
2020-04-28 23:46:38 -0700

    finds index of search value

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int a = 0;
+
+        foreach (int b in myLList)
+        {
+            if (b == value)
+            {
+                return a;
+            }
+
+            a++;
+        }
+
+        return -1;
+    }
+}
