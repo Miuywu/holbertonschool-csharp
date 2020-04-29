45e842224298055e034b11ba3b3c6749ffdad253
Miuywu <859@holbertonschool.com>
2020-04-28 23:13:15 -0700

    first commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        int yum = 0;
+        HashSet<int> hash = new HashSet<int>(myList);
+        foreach (int browns in hash)
+        {
+            yum += browns;
+        }
+        return yum;
+    }
+}
