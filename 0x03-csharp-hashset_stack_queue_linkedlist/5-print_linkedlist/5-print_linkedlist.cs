41f241b96dba282d2731f5937c583a2d453c336e
Miuywu <859@holbertonschool.com>
2020-04-28 23:41:03 -0700

    linked list addlast

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> lin = new LinkedList<int>();
+
+        for (int a = 0; a < size; a++)
+        {
+            Console.WriteLine(a);
+            lin.AddLast(a);
+        }
+        return lin;
+    }
+}
