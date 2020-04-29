b45b6cafde13c8b1cc81a4333b038974223a0884
Miuywu <859@holbertonschool.com>
2020-04-28 23:59:04 -0700

    summer nodes

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int a = 0;
+
+        foreach (int b in myLList)
+        {
+            a += b
+
+        return a;
+    }
+}
