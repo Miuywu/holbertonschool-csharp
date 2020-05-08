6fdfedbbcb3c0109a294590dac5a875f87da15fd
Miuywu <859@holbertonschool.com>
2020-05-07 23:00:15 -0700

    if array

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary> check obj type</summary>
+class Obj
+{
+    /// <summary>Check if instance of an Array.</summary>
+    /// <param name="obj">obj</param>
+    /// <returns>True if is instance.</returns>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        return obj is Array;
+    }
+}
