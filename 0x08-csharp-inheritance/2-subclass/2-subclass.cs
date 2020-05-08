16817ec9e9191cbc0dd3c605567b645038e7e7a7
Miuywu <859@holbertonschool.com>
2020-05-07 23:05:03 -0700

    if subclass

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>check object type.</summary>
+class Obj
+{
+    /// <summary>Checks if is subclass of another type.</summary>
+    /// <param name="derivedType">d type.</param>
+    /// <param name="baseType">b type.</param>
+    /// <returns>True if the derived type is a subclass.</returns>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        return derivedType.IsSubclassOf(baseType);
+    }
+}
