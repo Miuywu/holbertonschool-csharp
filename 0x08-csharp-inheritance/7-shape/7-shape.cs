35ac70139ad07cf613f3f2b432336b49eab4f3bb
Miuywu <859@holbertonschool.com>
2020-05-07 23:45:49 -0700

    new area and new str rep

---
+++ 0x08-csharp-inheritance/7-shape/7-shape.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+/// <summary>gets a shape.</summary>
+class Shape
+{
+    /// <summary>gets area.</summary>
+    /// <returns>area.</returns>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+/// <summary>Rectangle.</summary>
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    /// <summary>Gets/sets width.</summary>
+    /// <value>set value.</value>
+    public int Width
+    {
+        get
+        {
+            return width;
+        }
+        set
+        {
+            if (value >= 0)
+            {
+                width = value;
+            }
+            else
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            }
+        }
+    }
+
+    /// <summary>Gets/sets height.</summary>
+    /// <value>set value.</value>
+    public int Height
+    {
+        get
+        {
+            return height;
+        }
+        set
+        {
+            if (value >= 0)
+            {
+                height = value;
+            }
+            else
+            {
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            }
+        }
+    }
+}
+
+    /// <summary>new area of the Rectangle.</summary>
+    /// <returns>new area.</returns>
+    public new int Area()
+    {
+        return Height * Width;
+    }
+
+    /// <summary>new string representation of the Rectangle.</summary>
+    /// <returns>new string representation.</returns>
+    public override string ToString()
+    {
+        return String.Format("[Rectangle] {0} / {1}", Width, Height); 
+    }
+}
