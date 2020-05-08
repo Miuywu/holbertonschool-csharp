25d329e62490ced02c20570960502105a6766879
Miuywu <859@holbertonschool.com>
2020-05-07 23:33:04 -0700

    shape w/ not woking area

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+/// <summary>gets a shape.</summary>
+class Shape
+{
+    /// <summary>gets area.</summary>
+    /// <returns>area.</returns>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
