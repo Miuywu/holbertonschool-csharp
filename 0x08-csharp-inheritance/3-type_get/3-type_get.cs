e8af36ca0199a271e61a60f2ed6701f820f334de
Miuywu <859@holbertonschool.com>
2020-05-07 23:10:22 -0700

    methods and properties

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+/// <summary>type functions.</summary>
+class Obj
+{
+    /// <summary> displays properties and methods.</summary>
+    /// <param name="myObj">The obj.</param>
+    public static void Print(object myObj)
+    {
+        Type t = myObj.GetType();
+        PropertyInfo[] p = objType.GetProperties();
+        MethodInfo[] m = objType.GetMethods();
+
+        Console.WriteLine("{0} Properties:", objType.Name);
+        foreach (PropertyInfo property in p)
+        {
+            Console.WriteLine(property.Name);
+        }
+
+        Console.WriteLine("{0} Methods:", objType.Name);
+        foreach (MethodInfo method in m
+        {
+            Console.WriteLine(method.Name);
+        }
+
+    }
+}
