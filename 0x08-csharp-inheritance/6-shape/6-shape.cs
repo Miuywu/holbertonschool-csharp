77bdd05c82af25298234c4fd86cf91174f2baaac
Miuywu <859@holbertonschool.com>
2020-05-07 23:40:36 -0700

    shape witdth height

---
+++ 0x08-csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+/// <summary>gets a shape.</summary>
+class Shape
+{
+    /// <summary>gets area.</summary>
+    /// <returns>area.</returns>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+/// <summary>Rectangle.</summary>
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    /// <summary>Gets/sets width.</summary>
+    /// <value>set value.</value>
+    public int Width
+    {
+        get
+        {
+            return width;
+        }
+        set
+        {
+            if (value >= 0)
+            {
+                width = value;
+            }
+            else
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            }
+        }
+    }
+
+    /// <summary>Gets/sets height.</summary>
+    /// <value>set value.</value>
+    public int Height
+    {
+        get
+        {
+            return height;
+        }
+        set
+        {
+            if (value >= 0)
+            {
+                height = value;
+            }
+            else
+            {
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            }
+        }
+    }
+}
