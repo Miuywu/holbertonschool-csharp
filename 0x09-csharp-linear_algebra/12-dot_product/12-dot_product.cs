53dac91495347b68c9c4e008ffa50b501a74c010
Miuywu <859@holbertonschool.com>
2020-05-28 01:44:01 -0400

    10-18

---
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+///<summary>Class for VectorMath</summary>
+class VectorMath
+{
+	///<summary>vector dot product</summary>
+	///<return>dot product or -1</return>
+	public static double DotProduct(double[] vector1, double[] vector2)
+	{
+		if (vector1.Length == 2 && vector2.Length == 2)
+		{
+			return vector1[0] * vector2[0] + vector1[1] * vector2[1];
+		}
+		else if (vector1.Length == 3 && vector2.Length == 3)
+		{
+			return vector1[0] * vector2[0] + vector1[1] * vector2[1] + vector1[2] * vector2[2];
+		}
+		else
+		{
+			return -1;
+		}
+	}
+}
---
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+///<summary>Class for matrix math</summary>
+class MatrixMath
+{
+	///<summary>matrix multiplication</summary>
+	///<return>the result matrix or error</return>
+	public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+	{
+		if (matrix1.GetLength(1) == matrix2.GetLength(0))
+		{
+			double[,] result = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
+			for (int a = 0; a < matrix1.GetLength(0); a++)
+				for (int b = 0; b < matrix2.GetLength(1); b++)
+					for (int c = 0; c < matrix1.GetLength(1); c++)
+						result[i, j] += matrix1[i, k] * matrix2[k, j];
+			return result;
+		}
+		return new double[,] {{-1}};
+	}
+}
