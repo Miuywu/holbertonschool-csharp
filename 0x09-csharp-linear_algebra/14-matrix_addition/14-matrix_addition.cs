9bf7461cdb000e2a24e42083d46bc84c579ad073
Miuywu <859@holbertonschool.com>
2020-05-28 01:45:40 -0400

    14 16

---
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+///<summary>Class for MatrixMath</summary>
+class MatrixMath
+{
+	///<summary>calcuating matrix addition</summary>
+	///<return>result matrix or error</return>
+	public static double[,] Add(double[,] matrix1, double[,] matrix2)
+	{
+		if (matrix1.GetLength(0) == 2 && matrix1.GetLength(1) == 2 &&
+		matrix2.GetLength(0) == 2 && matrix2.GetLength(1) == 2)
+		{
+			double[,] result = {
+				{matrix1[0, 0] + matrix2[0, 0], matrix1[0, 1] + matrix2[0, 1]},
+				{matrix1[1, 0] + matrix2[1, 0], matrix1[1, 1] + matrix2[1, 1]}
+			};
+			return result;
+		}
+		else if (matrix1.GetLength(0) == 3 && matrix1.GetLength(1) == 3 &&
+		matrix2.GetLength(0) == 3 && matrix2.GetLength(1) == 3)
+		{
+			double[,] result = {
+				{matrix1[0, 0] + matrix2[0, 0], matrix1[0, 1] + matrix2[0, 1], matrix1[0, 2] + matrix2[0, 2]},
+				{matrix1[1, 0] + matrix2[1, 0], matrix1[1, 1] + matrix2[1, 1], matrix1[1, 2] + matrix2[1, 2]},
+				{matrix1[2, 0] + matrix2[2, 0], matrix1[2, 1] + matrix2[2, 1], matrix1[2, 2] + matrix2[2, 2]}
+			};
+			return result;
+		}
+		return new double[,] {{-1}};
+	}
+}
---
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+///<summary>Class for matrix math</summary>
+class MatrixMath
+{
+	///<summary>calcuating applying scalar on matrix</summary>
+	///<return>the result matrix or matrix containing -1</return>
+	public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+	{
+		if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
+		{
+			double[,] result = {
+				{matrix[0, 0] * scalar, matrix[0, 1] * scalar},
+				{matrix[1, 0] * scalar, matrix[1, 1] * scalar}
+			};
+			return result;
+		}
+		else if (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3)
+		{
+			double[,] result = {
+				{matrix[0, 0] * scalar, matrix[0, 1] * scalar, matrix[0, 2] * scalar},
+				{matrix[1, 0] * scalar, matrix[1, 1] * scalar, matrix[1, 2] * scalar},
+				{matrix[2, 0] * scalar, matrix[2, 1] * scalar, matrix[2, 2] * scalar}
+			};
+			return result;
+		}
+		return new double[,] {{-1}};
+	}
+}
