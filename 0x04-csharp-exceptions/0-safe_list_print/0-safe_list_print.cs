96fe3f868ad1ef3dff7217d935a6f5985dfa5f19
Minh Huy <859@hobertonschool.com>
2020-06-20 08:03:25 -0400

    exxceptions try catch finally

--- 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -1,25 +1,21 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 class List
 {
-	public static int SafePrint(List<int> myList, int n)
-	{
-		int a = 0;
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int size = 0;
 
-		try
-		{
-			for (int b = 0; b < n; b++)
-			{
-				Console.WriteLine(myList[b]);
-				a++;
-			}
-
-			return a;
-		}
-		catch
+        try
         {
-			return a;
+            for (int a = 0; a < n; a++, size++)
+                Console.WriteLine(myList[a]);
+            return size;
         }
-	}
+        catch (System.Exception)
+        {
+            return size;
+        }
+    }
 }
---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        int q = 0;
+
+        try
+        {
+            q = a / b;
+        }
+        catch
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally
+        {
+            Console.WriteLine("{0} / {1} = {2}", a, b, q);
+        }
+    }
+}
